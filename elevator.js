315fd05420170d9df2af8a8fa0636f2448093448
Pascal Grange <pascal@grange.nom.fr>
2013-09-29 15:14:31 +0200

    CHG étages triés par hauteur... mais du coup on revient trop souvent au début

--- elevator.js
+++ elevator.js
@@ -11,9 +11,9 @@ function new_state(values) {
 }
 
 function call(state, floor) {
-  if (state.path.last() != floor) {
+  if (! state.path.contains(floor)) 
     state.path.push(floor)
-  }
+  state.path.sort()
   return state
 }
 
