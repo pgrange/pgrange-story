84428cea0a9fa4b0c6685aad1565f5a44df7f9dc
Pascal Grange <pascal@grange.nom.fr>
2013-10-07 22:22:59 +0200

    suppressing expect.js dependency but adding mocha

--- test.js
+++ test.js
@@ -1,49 +1,52 @@
 var assert = require('assert');
-var expect = require('expect.js');
 var elevator = require('./elevator.js');
 var ns = elevator.new_state
 
 require('./utils.js')
 
-var state
-var next_state
-var result
+function assert_path_when_called_at(state, floor, expected_path) {
+  assert.deepEqual(elevator.call(state, floor).path, expected_path)
+}
 
-// add next target
-next_state = elevator.call(ns({floor: 0}), 1)
-expect(next_state.path).to.eql([1])
-
-// add next target event if at floor
-next_state = elevator.call(ns({floor: 0}), 0)
-expect(next_state.path).to.eql([0])
-
-// do not add if already last target
-next_state = elevator.call(ns({floor: 0}), 0)
-next_state = elevator.call(next_state, 0)
-expect(next_state.path).to.eql([0])
-
-// do not move if nowhere to go
-result = elevator.next(ns())
-expect(result.cmd).to.eql('NOTHING')
-
-// open then close if at next floor
-state = ns({floor: 0, path: [0, 1]})
-result = elevator.next(state)
-expect(result.cmd).to.eql('OPEN')
-expect(result.state.path).to.eql([1])
-
-result = elevator.next(state)
-expect(result.cmd).to.eql('CLOSE')
-expect(result.state.open).not.to.be.ok()
-
-// goes toward next floor (UP)
-state = ns({floor: 0, path: [1, 2]})
-result = elevator.next(state)
-expect(result.cmd).to.eql('UP')
-expect(result.state.floor).to.eql(1)
-
-// goes toward next floor (DOWN)
-state = ns({floor: 6, path: [1, 2]})
-result = elevator.next(state)
-expect(result.cmd).to.eql('DOWN')
-expect(result.state.floor).to.eql(5)
+describe('Elevator', function() {
+ describe('when called', function() {
+  it('should target floor', function() {
+    assert_path_when_called_at(ns(), 1, [1])
+  })
+  it('should target floor event if it is current floor', function() {
+    assert_path_when_called_at(ns({floor: 0}), 0, [0])
+  })
+  it('should ignore if already targets floor', function() {
+    var state = elevator.call(ns({floor: 0}), 0)
+    assert_path_when_called_at(state, 0, [0])
+  })
+ })
+ describe('when asked what to do next', function() {
+  it('should not move if nowhere to go', function() {
+    var result = elevator.next(ns())
+    assert.equal(result.cmd, "NOTHING")
+  })
+  it('should go up toward next floor', function() {
+    var state = ns({floor: 0, path: [1, 2]})
+    var result = elevator.next(state)
+    assert.equal(result.cmd, "UP")
+    assert.equal(result.state.floor, 1)
+  })
+  it('should go down toward next floor', function() {
+    var state = ns({floor: 6, path: [1, 2]})
+    var result = elevator.next(state)
+    assert.equal(result.cmd, "DOWN")
+    assert.equal(result.state.floor, 5)
+  })
+  it('should open then close if at targeted floor', function() {
+    var state = ns({floor: 0, path: [0, 1]})
+    var result = elevator.next(state)
+    assert.equal(result.cmd, "OPEN")
+    assert.deepEqual(result.state.path, [1])
+    
+    result = elevator.next(result.state)
+    assert.equal(result.cmd, "CLOSE")
+    assert.equal(result.state.open, false)
+  })
+ })
+})
